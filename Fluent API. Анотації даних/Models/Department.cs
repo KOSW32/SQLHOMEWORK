2673f359d6292915cd461bebf419a25b944a9ba2
KOSW32 <119171476+KOSW32@users.noreply.github.com>
2026-09-11 00:10:29 +0300

    Delete Fluent API. Анотації даних directory

--- Fluent API. Анотації даних/Models/Department.cs
+++
@@ -1,9 +0,0 @@
-namespace AcademyApp.Models;
-
-public class Department
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public List<Subject> Subjects { get; set; } = new();
-}
--- Fluent API. Анотації даних/Models/Group.cs
+++
@@ -1,9 +0,0 @@
-namespace AcademyApp.Models;
-
-public class Group
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public List<Student> Students { get; set; } = new();
-}
